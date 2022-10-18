db862f4c20f63fc9a1768308b708210211271420
IGustiNgurahKadekRianAdnyana <114999037+IGustiNgurahKadekRianAdnyana@users.noreply.github.com>
2022-10-19 04:26:02 +0700

    Update Bisection Method.cpp

--- Bisection Method.cpp
+++ Bisection Method.cpp
@@ -1,57 +1,87 @@
-//bisection method
-#include<iostream>
-#include<cmath>
-#include<iomanip>
+#include <iostream>
+#include <windows.h>
+#include <math.h>
 using namespace std;
-double f(double x);    //declare the function for the given equation
-double f(double x)    //define the function here, ie give the equation
-{
-    double a=pow(x,3)-x-11.0;    //write the equation whose roots are to be determined
-    return a;
+
+double func(double x, double n, double k[], double c){
+	double sum, sum2 = 0;
+	for(int i=0; i<n; i++){
+		sum = 1;
+		sum *= k[i];
+		for(int j=n; j>i; j--){
+			sum *= x;
+		}
+		sum2 += sum;
+	}
+	
+	sum2 += c;
+	
+	return sum2;
 }
-int main()
-{    
-    cout.precision(4);        //set the precision
-    cout.setf(ios::fixed);
-    double a,b,c,e,fa,fb,fc;    //declare some needed variables
-    a:cout<<"Enter the initial guesses:\na=";    //Enter the value of a(set a label('a:') for later use with goto)
-    cin>>a;
-    cout<<"\nb=";            //Enter the value of b
-    cin>>b;
-    cout<<"\nEnter the degree of accuracy desired"<<endl;    //Enter the accuracy
-    cin>>e;                //e stands for  accuracy
-    if (f(a)*f(b)>0)        //Check if a root exists between a and b
-    {                //If f(a)*f(b)>0 then the root does not exist between a and b
-        cout<<"Please enter a different intial guess"<<endl;
-        goto a;            //go back to 'a' ie 17 and ask for different values of a and b
-    }
-    else                //else a root exists between a and b
-    {
-    while (fabs(a-b)>=e)        /*if the mod of a-b is greater than the accuracy desired keep                         bisecting the interval*/
-    {
-        c=(a+b)/2.0;        //bisect the interval and find the value of c
-        fa=f(a);        
-        fb=f(b);
-        fc=f(c);
-        cout<<"a="<<a<<"     "<<"b="<<b<<"     "<<"c="<<c<<"      fc="<<fc<<endl;/*print the                             values of a,b,c and fc  after each iteration*/       
-        if (fc==0)        //if f(c)=0, that means we have found the root of the equation
-        {
-            cout<<"The root of the equation is "<<c;    /*print the root of the equation                                         and break out of the loop*/
-            break;
-        }
- 
-        if (fa*fc>0)    //if f(a)xf(c)>0, that means no root exist between a and c 
-        {
-            a=c;    /*hence make a=c, ie make c the starting point of the interval and b the                     end point*/
-        }
-        else if (fa*fc<0)
-        {    
-            b=c;    /*this means that a root exist between a and c therfore make c the end                     point of the interval*/
-        }    
-         
-     
-    }
-    }            //The loop ends when the difference between a and b becomes less than the desired accuracy ie now the value stored in 'c' can be called the approximate root of the equation         
-    cout<<"The root of the equation is "<<c;    //print the root    
-    return 0;    
+
+int main(){
+	system("cls");
+	
+	double n, k[50], c, bAtas, bBawah, bC, error, input_error;
+	int i = 0;
+	
+	cout <<"Masukan n / pangkat tertinggi : ";
+	cin >> n;
+	for (int i=0; i<n; i++){
+		cout << "Masukan koefisien (k"<< i+1 << ") dari x^" << n-i <<" : ";
+		cin >> k[i];
+	}
+	cout << "Masukan c (konstanta) : ";
+	cin >> c;
+	
+	cout << "Masukan Batas Bawah : ";
+	cin >> bBawah;
+	cout <<"Masukan Batas Atas : ";
+	cin >> bAtas;
+	
+	cout << "Masukan nilai error yang diinginkan : ";
+	cin >> input_error;
+	
+	if(func(bBawah,n,k,c) * func(bAtas,n,k,c) >= 0){
+		cout << "\nProses dibatalkan, Anda tidak memasukan bAtas dan bBawah dengan benar sesuai teori" << endl;
+		return 0;
+	}
+	else if (func(bBawah,n,k,c) == 0 || func(bAtas,n,k,c) == 0)
+	{
+		cout <<"Akar merupakan salah satu dari batas atas dan bawah" << endl;
+		cout <<"Akar persamaan = " << (func(bAtas,n,k,c)==0?bAtas:bBawah) <<endl;
+	}
+	
+	cout << "Ite\ta\t\tb\t\tf(a)\t\tf(b)\t\tc\t\tf(c)\t\terror\n" << endl;
+	
+	double mC;
+	
+	do{
+		mC = bC;
+		bC = (bBawah+bAtas)/2; 
+		
+		cout << ++i << "\t" << bBawah << "\t\t" << bAtas << "\t\t" << (func(bBawah,n,k,c)) << "\t\t" << (func(bAtas,n,k,c)) << "\t\t" << bC << "\t\t" << (func(bC,n,k,c)) << endl;
+		
+		if(func(bC,n,k,c) == 0){
+			cout << "Akar persamaan : " << bC << endl;
+		} else if ((func(bBawah,n,k,c)*func(bC,n,k,c)) > 0 ){
+			bBawah = bC;
+		} else {
+			bAtas = bC;
+		}
+		
+		error = fabs(bC-mC);
+		
+		if(i==1)
+		{
+			cout << "_ _ _ _" << endl;
+		} else
+		{
+			cout << error << endl;
+		}
+		 
+	}while (error > input_error);
+	cout << "\n\nAproksimasi Akar = " << bC <<endl;
+	
+	return 0;
 }
